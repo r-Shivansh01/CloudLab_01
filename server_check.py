c17c56b5426faf082b0ee9f84ccdb14a35585e1d
Shivansh Rao. <149760297+r-Shivansh01@users.noreply.github.com>
2025-12-11 08:45:47 +0530

    Create server_check.py
    python server health check script added

---
+++ server_check.py
@@ -0,0 +1,13 @@
+import requests
+
+def check_server(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"Success! {url} is Online.")
+        else:
+            print(f"Alert! {url} returned {response.status_code}")
+    except:
+        print(f"Critical: Could not connect to {url}")
+
+check_server("https://www.google.com")
